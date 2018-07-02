69482fd8344f42b2faada156f80a019445bcdefe
Dell <farros@rambler.ru>
2018-07-02 17:50:13 +0300

    commit

--- src/app/options-page/options-page.component.ts
+++ src/app/options-page/options-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-options-page',
   templateUrl: './options-page.component.html',
-  styleUrls: ['./options-page.component.css']
+  styleUrls: ['./options-page.component.css'],
 })
 export class OptionsPageComponent implements OnInit {
 
